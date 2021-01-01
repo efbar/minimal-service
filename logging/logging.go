99b9a15586f5209806f8683eba7b3466ec59629c
efbar <17676301+efbar@users.noreply.github.com>
2021-01-01 18:36:19 +0100

    add  functionalities and color to logging

--- logging/logging.go
+++ logging/logging.go
@@ -4,6 +4,13 @@ import (
 	"log"
 )
 
+const (
+	reset  = "\033[0m"
+	red    = "\033[31m"
+	green  = "\033[32m"
+	yellow = "\033[33m"
+)
+
 // Logger ...
 type Logger struct {
 	Logger *log.Logger
@@ -11,5 +18,15 @@ type Logger struct {
 
 // Info ...
 func (l Logger) Info(params ...string) {
-	l.Logger.Println(params)
+	l.Logger.Printf("%s [INFO] %s %s", yellow, params, reset)
+}
+
+// Error ...
+func (l Logger) Error(params ...string) {
+	l.Logger.Printf("%s [ERROR] %s %s", red, params, reset)
+}
+
+// Debug ...
+func (l Logger) Debug(params ...string) {
+	l.Logger.Printf("%s [DEBUG] %s %s", green, params, reset)
 }
