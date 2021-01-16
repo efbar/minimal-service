291004d19240b7a795ef20d4cdeb3ba65896f36a
efbar <17676301+efbar@users.noreply.github.com>
2021-01-16 18:34:28 +0100

    better consul connect configuration

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"log"
+	"net"
 	"net/http"
 	"os"
 	"os/signal"
@@ -18,31 +19,35 @@ import (
 )
 
 func main() {
-	var client *consul.Client
+
+	// create a logger object
 	l := log.New(os.Stdout,
 		"Logger: ",
 		log.Ldate|log.Ltime)
 	logger := &logging.Logger{
 		Logger: l,
 	}
+
+	// fill envs
 	envs := helpers.ListEnvs
 
+	// set service port
 	port := envs["SERVICE_PORT"]
-	if port == "" {
-		port = "9090"
-	}
 
+	// create http requests handlers
 	anyReq := handlers.HandlerAnyHTTP(*logger, envs)
 	bounceReq := handlers.HandlerBounceHTTP(*logger, envs)
 	healthReq := handlers.HandlerHealth(*logger, envs)
 
+	// create server mux
 	sm := http.NewServeMux()
 
+	// assign handler to paths
 	sm.Handle("/", anyReq)
 	sm.Handle("/bounce", bounceReq)
 	sm.Handle("/health", healthReq)
 
-	// create a new server
+	// fill the new server config
 	s := http.Server{
 		Addr:         ":" + port,
 		Handler:      sm,
@@ -52,19 +57,13 @@ func main() {
 		IdleTimeout:  120 * time.Second,
 	}
 
-	if len(envs["CONNECT"]) == 0 {
-		envs["CONNECT"] = "false"
-	}
-	connectActive, err := strconv.ParseBool(envs["CONNECT"])
-	if err != nil {
-		logger.Error("Error from consul,", err.Error())
-	}
-
-	if connectActive {
+	// if consul connect enabled, connect to it
+	var client *consul.Client
+	if envs["CONNECT"] == "1" {
 		client = connectToConsul(&s, envs, logger)
 	}
 
-	// run it
+	// run the http server
 	go func() {
 		logger.Info("Starting server on port " + port)
 
@@ -75,7 +74,7 @@ func main() {
 		}
 	}()
 
-	// get sigterm or interupt
+	// get terminal and syscall signals
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, syscall.SIGKILL)
 	signal.Notify(c, syscall.SIGTERM)
@@ -86,13 +85,15 @@ func main() {
 	sig := <-c
 	logger.Info("Got signal:", sig.String())
 
+	// if we are here and consul connect is active, deregister the service from it
 	if err := client.Agent().ServiceDeregister("minimal-service"); err != nil {
 		logger.Error(err.Error())
 	}
-	if connectActive {
+	if envs["CONNECT"] == "1" {
 		logger.Debug("Consul service deregistration")
 	}
 
+	// gracefully shutdown if connections are active
 	// wait max 3 seconds before shutdown
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
@@ -101,38 +102,78 @@ func main() {
 
 func connectToConsul(s *http.Server, envs map[string]string, logger *logging.Logger) *consul.Client {
 
-	if len(envs["CONSUL_SERVER"]) == 0 {
-		envs["CONSUL_SERVER"] = "127.0.0.1:8500"
+	logger.Debug(envs["DEBUG"], "CONSUL_SERVER:", envs["CONSUL_SERVER"])
+
+	// fill some vars if we are in kube
+	kubeNode := os.Getenv("HOST_IP")
+	kubePod := os.Getenv("POD_NAME")
+	var host string
+	if len(kubeNode) != 0 {
+		envs["CONSUL_SERVER"] = kubeNode + ":8500"
+		host = kubePod
+	} else {
+		host, _ = helpers.GetHostname()
 	}
+
+	// set client configs
 	conf := &consul.Config{
-		Address:    envs["CONSUL_SERVER"],
-		Transport:  &http.Transport{},
-		HttpClient: &http.Client{},
-		HttpAuth:   &consul.HttpBasicAuth{},
-		WaitTime:   0,
-		TLSConfig:  consul.TLSConfig{},
+		Address:  envs["CONSUL_SERVER"],
+		WaitTime: 0,
 	}
+
+	// create client and service
 	client, _ := consul.NewClient(conf)
 	svc, _ := connect.NewService("minimal-service", client)
 	defer svc.Close()
 	s.TLSConfig = svc.ServerTLSConfig()
 
+	// set service details: serviceID, port, tags, address, meta tags
 	serviceID := "minimal-service"
-	if len(envs["SERVICE_PORT"]) == 0 {
-		envs["SERVICE_PORT"] = "9090"
-	}
 	port, _ := strconv.Atoi(envs["SERVICE_PORT"])
 	tags := []string{"microservice", "http"}
+
+	addresses, err := net.InterfaceAddrs()
+	if err != nil {
+		logger.Error(err.Error())
+	}
+	var ipAddr string
+	for _, address := range addresses {
+		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				ipAddr = ipnet.IP.String()
+			}
+		}
+	}
+
+	meta := map[string]string{}
+	if len(kubeNode) != 0 {
+		meta = map[string]string{
+			"pod-name": os.Getenv("POD_NAME"),
+			"version":  "v1",
+		}
+	} else {
+		meta = map[string]string{
+			"hostname": host,
+			"version":  "v1",
+		}
+	}
+
+	// fill service registration, set native connect, set service check
 	service := &consul.AgentServiceRegistration{
-		ID:      serviceID,
+		ID:      host + "-" + serviceID,
 		Name:    serviceID,
 		Port:    port,
-		Address: serviceID,
+		Address: ipAddr,
 		Tags:    tags,
+		Meta:    meta,
+		Connect: &consul.AgentServiceConnect{
+			Native: true,
+		},
 		Check: &consul.AgentServiceCheck{
-			HTTP:     "http://" + serviceID + ":" + envs["SERVICE_PORT"] + "/health",
-			Interval: "5s",
-			Timeout:  "1s",
+			HTTP:                           "http://" + ipAddr + ":" + envs["SERVICE_PORT"] + "/health",
+			Interval:                       "5s",
+			Timeout:                        "1s",
+			DeregisterCriticalServiceAfter: "1m",
 		},
 	}
 
